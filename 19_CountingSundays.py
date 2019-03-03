204c2b256a57c32287f62c2163bfabffb8b0ab62
Matthew <matthewbuckley051@gmail.com>
2019-03-03 16:15:41 +0000

    Added some bits to problem 19, although still WIP.

--- 19_CountingSundays.py
+++ 19_CountingSundays.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Mar 26 21:56:31 2018
-
-@author: Matthew
 Project Euler Problem 19 Counting Sundays
+WIP - Still working on this problem.
 """
+##### IMPORTS #####
 
+##### FUNCTIONS #####
 def isLeap(year):
     """
     Checks if the year given is a leap year and returns bool.
@@ -20,7 +20,23 @@ def isLeap(year):
     else:
         return False
 
+def calcDays(curDate, curMonth, curYear):
+    """
+	Calculates the number of days since the previous Sunday.
+    """
+    # Calculate the number of days since the previous Sunday and if that number is divisible by 7 then today is also a Sunday.
+    # Change in date since previous Sunday
+    dateDiff = curDate - startDate
+    # Months passed since previous Sunday
+    monthDays = 0
+    while curMonth != sunMonth:
+        # Need to loop through the months adding up the days as we go
+	pass
+    # Then need to loop through the years and add them up to
+    return
 
+##### MAIN #####
+# Number of days per month (regular year)
 daysMonth = {
         "Jan":31,
         "Feb":28,
@@ -35,7 +51,15 @@ daysMonth = {
         "Nov":30,
         "Dec":31
         }
+# List of the reverse order of months
+months = ['Dec', 'Nov', 'Oct', 'Sep', 'Aug', 'Jul', 'Jun', 'May', 'Apr', 'Mar', 'Feb', 'Jan']
 
-currentYear = 1901
-currentMonth = "Jan"
-currentDay = 1
+# Variables to keep track of date of previous Sunday starting on first Sunday in 20th century
+sunYr = 1900
+sunMonth = "Jan"
+sunDate = 7
+
+# Current date, starting in Feb 1900 (we know 1st Jan 1900 is Monday)
+curYr = 1900
+curMonth = "Feb"
+curDate = 1
